6d1b58c277cfc1b35561b75ba8637b6dd4ae3214
sakoda <sakoda@keio.jp>
2021-11-11 23:30:13 +0900

    プロット用スクリプトを実装

---
+++ plot.py
@@ -0,0 +1,20 @@
+from os import sep
+import matplotlib.pyplot as plt
+import glob
+import natsort
+import pandas as pd
+import japanize_matplotlib
+
+loss_log_list = glob.glob("loss_log_*.txt")
+loss_log_list = natsort.natsorted(loss_log_list)
+
+for i, loss_log in enumerate(loss_log_list):
+    df = pd.read_csv(loss_log, sep="\t", header=None)
+    df = df.set_axis(["step", "accuracy", "reward", "loss"], axis="columns")
+    plt.plot(df["step"], df["accuracy"], label=f"{i}回目の試行")
+
+plt.xlabel("Learning Step")
+plt.ylabel("Accuracy")
+plt.ylim((0, 1))
+plt.legend()
+plt.savefig("accuracy.png", bbox_inches="tight", pad_inches=0.05)
