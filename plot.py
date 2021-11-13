a7ce638da74c5fe731e0b77d3f30d32209194697
sakoda <sakoda@keio.jp>
2021-11-13 12:26:29 +0900

    プロット用スクリプト修正

--- plot.py
+++ plot.py
@@ -11,10 +11,10 @@ loss_log_list = natsort.natsorted(loss_log_list)
 for i, loss_log in enumerate(loss_log_list):
     df = pd.read_csv(loss_log, sep="\t", header=None)
     df = df.set_axis(["step", "accuracy", "reward", "loss"], axis="columns")
-    plt.plot(df["step"], df["accuracy"], label=f"{i}回目の試行")
+    plt.plot(df["step"], df["accuracy"], label=f"{i + 1:2d}回目の試行")
 
 plt.xlabel("Learning Step")
 plt.ylabel("Accuracy")
 plt.ylim((0, 1))
-plt.legend()
+plt.legend(bbox_to_anchor=(1.025, 1), loc='upper left', borderaxespad=0)
 plt.savefig("accuracy.png", bbox_inches="tight", pad_inches=0.05)
